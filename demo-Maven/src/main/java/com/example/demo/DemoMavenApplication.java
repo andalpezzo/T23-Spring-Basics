fd32064fa859a6721361d71e001a201c56025fda
andalpezzo <andalpezzo@gmail.com>
2023-04-17 14:19:58 +0200

    Commiteando

---
+++ demo-Maven/src/main/java/com/example/demo/DemoMavenApplication.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMavenApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String welcome() {
+		return String.format("Bienvenido a Spring Framework");
+	}
+	
+	@GetMapping("/hello")
+    public String hello() {
+      return String.format("Hola mundo");
+    }
+	
+	@GetMapping("/saluda")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
+}
